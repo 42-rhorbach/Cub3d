0bf2b733a3ea27eb97f9c854a098a8c3e2d89c6d
Jan Gijs Vorstman <jvorstma@f1r4s11.codam.nl>
2024-01-18 18:58:58 +0100

    start read and parse file

---
+++ src/read.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvorstma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 18:48:25 by jvorstma          #+#    #+#             */
+/*   Updated: 2024/01/18 18:48:28 by jvorstma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "types.h"
+#include "libft.h"
+
+t_error ft_read_and_parse(int fd)
+{
+    //check NO, SO, WE, EA path, check F and C colour.
+    //then store map in 2d array, check if only '0', '1', ' '.
+    // and if only one ('N', 'S', 'W', 'E').
+    //make struct, so can check if still NULL or already filled in.
+    
+    //first check, then malloc?
+    // or malloc and meanwhile check
+
+    //check if map is enclosed
+    return (OK);
+}
